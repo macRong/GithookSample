d793bd992f38f6e0ec3cc9b81d5ce1bca08dcccd
macRong <121071838@qq.com>
2018-12-11 15:28:58 +0800

    Initial Commit

---
+++ GithookSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GithookSample
+//
+//  Created by macRong on 2018/12/11.
+//  Copyright © 2018 macRong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
